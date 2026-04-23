0ed8cd3bfcde5972736f23afc7c10c3808e4d34c
jagant141 <jagant141@gmail.com>
2026-04-23 12:34:49 +0530

    Update main.tf
    bucket edited

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ provider "aws" {
 region = "ap-south-2"
 }
 resource "aws_s3_bucket" "demo_bucket" {
-bucket = "jagan_bukt"
+bucket = "jagan-bukt"
 }
 resource "aws_vpc" "demo_vpc" {
 cidr_block = "10.0.0.0/16"
